b1a3239cd89e3901ec9689a7090ad0d0f47119d8
Pratik <pratiksalunke1905@gmail.com>
2025-06-05 16:24:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+import Programmes from './Components/Programmes/Programmes'
+import Title from './Components/Title/Title'
+import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
+
+function App() {
+  return(
+    <div>
+        <Navbar/>
+        <Hero/>
+        <div className='container'>
+          <Title subTitle='Our PROGRAM' title='What We Offer'/>
+          <Programmes/>
+          <About/>
+          <Title subTitle='Gallery' title='Campus Photos'/>
+          <Campus/>
+          <Title subTitle='TESTIMONIALS' title='What Student Says'/>
+          <Testimonials/>
+          <Title subTitle='Contact Us' title='Get in Touch'/>
+          <Contact/>
+          <Footer/>
+        </div>
+    </div>
+  )
+}
+
+export default App
